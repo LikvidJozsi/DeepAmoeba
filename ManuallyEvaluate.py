04ab82b0075da656607faf75f6d8b8b02c8a0c7d
BanocziDavid <bduvid@gmail.com>
2021-05-22 18:46:36 +0200

    neural network architecture tweaks for better learning. Convergence verified by two consecutive episodes of >90% winrate against random-agent CU-jx7feq

--- ManuallyEvaluate.py
+++ ManuallyEvaluate.py
@@ -8,7 +8,9 @@ from AmoebaPlayGround.HandWrittenAgent import HandWrittenAgent
 Amoeba.map_size = (15, 15)
 
 graphical_view = GraphicalView(Amoeba.map_size)
-#hand_written_agent = HandWrittenAgent()
-neural_agent = MCTSAgent(simulation_count=10)
-game = GameGroup(batch_size=1, x_agent=neural_agent, o_agent=graphical_view, view=graphical_view)
+hand_written_agent = HandWrittenAgent()
+neural_agent = MCTSAgent(simulation_count=100)
+game = GameGroup(batch_size=10, x_agent=neural_agent, o_agent=hand_written_agent, view=None,log_progress=True)
+game.play_all_games()
+game = GameGroup(batch_size=10, x_agent=neural_agent, o_agent=neural_agent, view=None,log_progress=True)
 game.play_all_games()
